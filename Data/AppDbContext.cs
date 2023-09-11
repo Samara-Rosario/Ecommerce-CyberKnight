6954c6b14601d6a13c514475a88a007c0d4e88d3
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-09-11 15:58:35 -0300

    feito a classe e os atributos da classe

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,12 @@
-﻿namespace Ecommerce_CyberKnight.Data {
-    public class AppDbContext {
+﻿using Ecommerce_CyberKnight.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_CyberKnight.Data {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Endereco> Enderecoes { get; set; } 
+
+
     }
 }
