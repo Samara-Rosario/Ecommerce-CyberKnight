4b62080d918f2a039b8f21e0d90d054db19911bf
RafaelG132 <rafael48245716@edu.pa.senac.br>
2023-09-11 15:59:51 -0300

    Add classe itemDoPedido

---
+++ Models/itemDoPedido.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce_CyberKnight.Models
+{
+    public class itemDoPedido
+    {
+        public int IdPedido { get; set; }
+        public int IdProduto { get; set; }
+        public string endereco { get; set; }
+        public string cliente { get; set; }
+        public int quantidade { get; set;}
+    }
+}
