6a88e53e5ec8a90bf0fee2a730d09ff7222ccebe
RafaelG132 <rafael48245716@edu.pa.senac.br>
2023-09-11 15:47:32 -0300

    Adição da classe pedido

---
+++ Models/Pedido.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce_CyberKnight.Models
+{
+    public class Pedido
+    {
+        public int Id {  get; set; }
+        public string cliente { get; set; }
+        public string endereco { get; set; }
+        public string itensDoPedido { get; set; }
+        public double formaPagamento { get; set; }
+        public DateTime DataeHora { get; set; }
+
+    }
+}
