bbc5779e2d9db1f7810fb4e3920560c5a0b22a22
Luis Oliveira <luis11003156@edu.pa.senac.br>
2023-09-11 15:57:26 -0300

    Terminei a classe Produto.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,13 @@
-﻿namespace Ecommerce_CyberKnight.Data {
-    public class AppDbContext {
+﻿using Ecommerce_CyberKnight.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ecommerce_CyberKnight.Data{
+    public class AppDbContext : DbContext{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        }
+    
     }
-}
+
