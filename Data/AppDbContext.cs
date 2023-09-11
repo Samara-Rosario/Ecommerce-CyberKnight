f7c45fb827caa7ebf1361a7b21cd104e4bb1928e
KetPR <kethellen48382566@edu.pa.senac.br>
2023-09-11 15:57:05 -0300

    add classe Clientes

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,10 @@
-﻿namespace Ecommerce_CyberKnight.Data {
-    public class AppDbContext {
+﻿using Ecommerce_CyberKnight.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_CyberKnight.Data {
+    public class AppDbContext : DbContext {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+        public DbSet<Clientes> Clientes { get; set; }
+    
     }
 }
