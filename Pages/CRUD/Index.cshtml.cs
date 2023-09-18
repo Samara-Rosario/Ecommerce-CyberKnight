1c258654512eb110d130cbcb6af183453ba39500
KetPR <kethellen48382566@edu.pa.senac.br>
2023-09-18 17:50:08 -0300

    digitando valores

---
+++ Pages/CRUD/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce_CyberKnight.Pages.CRUD
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
