0529d0180894a6e2d230e310ed4948c525a14586
Luis Oliveira <luis11003156@edu.pa.senac.br>
2023-09-19 17:50:40 -0300

    terminando o site...

---
+++ Pages/crud/Incluir.cshtml.cs
@@ -0,0 +1,26 @@
+using Ecommerce_CyberKnight.Data;
+using Ecommerce_CyberKnight.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce_CyberKnight.Pages.crud{
+    public class IncluirModel : PageModel{
+        private readonly ApplicationDbContext _context;
+        public IncluirModel(ApplicationDbContext context){
+            _context = context;
+        }
+
+        [BindProperty]
+        public Clientes cliente { get; set; }
+        public void OnGet(){
+
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+            return Page();
+
+        }
+    }
+}
--- Pages/crud/Index.cshtml.cs
+++ Pages/crud/Index.cshtml.cs
@@ -5,8 +5,11 @@ namespace Ecommerce_CyberKnight.Pages.crud
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public string Nome { get; set; }
+        public int? Num { get; set; }
+        public void OnGet(string nome, int? num){
+            Nome = nome;
+            Num = num;
         }
     }
 }
