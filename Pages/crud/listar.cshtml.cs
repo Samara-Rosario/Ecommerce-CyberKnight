2c735797b75b43fdf01a3c96b4a019272da8ce43
Luis Oliveira <luis11003156@edu.pa.senac.br>
2023-09-21 17:44:22 -0300

    terminamos metade da pagina, falta mais alguns incrementos

--- Pages/crud/listar.cshtml.cs
+++ Pages/crud/listar.cshtml.cs
@@ -2,6 +2,7 @@ using Ecommerce_CyberKnight.Data;
 using Ecommerce_CyberKnight.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Ecommerce_CyberKnight.Pages.crud
@@ -14,8 +15,23 @@ namespace Ecommerce_CyberKnight.Pages.crud
         }
 
         public IList<Clientes> Clientes { get; set; }
-        public void OnGet(){
+        public async Task<IActionResult> OnGet(){
+            Clientes = await _context.Clientes.ToListAsync();
 
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int? id){
+            //Busca no banco de dados o cliente com o mesmo id procurado
+            var Cliente = await _context.Clientes.FirstOrDefaultAsync( c => c. Id == id);
+
+            //Verifica se foi retornado algum cliente do banco de dados
+            if (Cliente != null){ 
+                _context.Clientes.Remove(Cliente);
+                await _context.SaveChangesAsync();
+            }
+            //Redireciona para a página de listagem de clientes
+            return RedirectToPage("./listar");
         }
     }
 }
