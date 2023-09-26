6b05768e2722294925fbbe288a9c049ecc7f00c0
KetPR <kethellen48382566@edu.pa.senac.br>
2023-09-26 17:54:02 -0300

    adição de clientes

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -6,14 +6,39 @@ namespace Ecommerce_CyberKnight.Models {
 
         [Key]
         public int? Id { get; set; }
+        
+        [Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[RegularExpression(@"[0-9]{11}", ErrorMessage = "O campo {0} deve ser preenchido com 11 digitos numéricos")]
         public string Cpf {  get; set; }
-        public int? IdEndereco { get; set; }
-        public string Nome {  get; set; }
-        public string Email {  get; set; }
-        public string Telefone {  get; set; }
-        public string Cep {  get; set; }
-        public string Login {  get; set; }
-        public string Senha {  get; set; }
+
+		public int? IdEndereco { get; set; }
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[MinLength(100, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		public string Nome {  get; set; }
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[EmailAddress(ErrorMessage = "O campo {0} deve conter um endereço de e-mail válido")]
+		public string Email {  get; set; }
+
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		public string Telefone {  get; set; }
+
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		public string Cep {  get; set; }
+
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		public string Login {  get; set; }
+
+		[Required(ErrorMessage = "O campo \"{0}\" é de preechimento obrigatório")]
+		[MinLength(11, ErrorMessage = "O campo {0} deve ter no mínimo {1} caracteres.")]
+		public string Senha {  get; set; }
 
    
         [ForeignKey("IdEndereco")]
