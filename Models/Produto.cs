bd9b93653f58f5031a7fa44271741935070f685f
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-09-26 17:54:33 -0300

    implementando o Data Annotation

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,12 +1,31 @@
-﻿namespace Ecommerce_CyberKnight.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce_CyberKnight.Models
 {
     public class Produto{
+        [Key]
         public int? Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Nome { get; set; }
+
+        [MaxLength(200, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string descricao { get; set; }
+
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string categoria { get; set; }
+
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public double preco {  get; set; }
+
+        [MaxLength(10, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public int unidadeMedida { get; set; }
+
+        [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public float estoque { get; set; }
 
         public ICollection<itemDoPedido> ItensDoPedido { get; set; }
