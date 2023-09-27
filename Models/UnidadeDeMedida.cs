ee48a8e5a2e57a94937dbe68530f8565df04ac3c
KetPR <kethellen48382566@edu.pa.senac.br>
2023-09-27 15:41:20 -0300

    adição de unidade de medida

---
+++ Models/UnidadeDeMedida.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce_CyberKnight.Models {
+	public class UnidadeMedida {
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+
+		[Required(ErrorMessage = "O campo \"{0}\" é obrigatório")]
+		public string Sigla { get; set; }
+
+
+		[Required(ErrorMessage = "O campo \"{0}\" é obrigatório")]
+		public string NomeExtenso { get; set; }
+	}
+}
