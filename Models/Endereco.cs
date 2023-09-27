c6af2b77c9524a4edaadab49dda16a0fb9addd16
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-09-27 15:16:33 -0300

    alterações

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -9,31 +9,31 @@ namespace Ecommerce_CyberKnight.Models {
         [Key]
         public int? Id { get; set; }
 
-        [MaxLength(8, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(8, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Cep { get; set; }
 
-        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Cidade { get; set;}
 
-        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Bairro { get; set; }
 
-        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Rua { get; set; }
 
-        [MaxLength(4, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(4, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public int Numero { get; set; }
 
-        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Logradouro { get; set; }
 
-        [MaxLength(150, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(150, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Complemento { get; set; }
     }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -6,22 +6,22 @@ namespace Ecommerce_CyberKnight.Models
         [Key]
         public int? Id { get; set; }
 
-        [MaxLength(100, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(100, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string Nome { get; set; }
 
-        [MaxLength(200, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(200, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string descricao { get; set; }
 
-        [MaxLength(50, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(50, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public string categoria { get; set; }
 
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public double preco {  get; set; }
 
-        [MaxLength(10, ErrorMessage = "O campo {0} deve ter tamanho igual a {1}")]
+        [MaxLength(10, ErrorMessage = "O campo \"{0}\" deve ter tamanho igual a {1}")]
         [Required(ErrorMessage = "O campo \"{0}\" é de preenchimento obrigatório.")]
         public int unidadeMedida { get; set; }
 
