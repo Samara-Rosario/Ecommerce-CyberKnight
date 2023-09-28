79c5d4633ddc5fb8d1f4fab1a3efde1a7c545805
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-09-28 15:45:48 -0300

    inclusão para usar

--- Pages/ProdutoCRUD/Alterar.cshtml.cs
+++ Pages/ProdutoCRUD/Alterar.cshtml.cs
@@ -13,23 +13,46 @@ namespace Ecommerce_CyberKnight.Pages.ProdutoCRUD
             _context = context;
         }
 
-        // o [BindProperty] configura a aplicação para relacionar o atributo 'cliente' aos dados que estão vindo do front-end*/
+        // o "[BindProperty]" configura a aplicação para relacionar o atributo 'cliente' aos dados que estão vindo do front-end*/
         [BindProperty]
         public Produto produtos { get; set; }
 
         public async Task<IActionResult> OnGet(int id){
+            if(id == null) {
+                return NotFound();
+            }
             produtos = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+
+            if(produtos == null) {
+                return NotFound();
+            }
 
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync() {
-
-
+            if (!ModelState.IsValid) {
+                return Page();
+            }
             _context.Attach(produtos).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+
+            try {
+                await _context.SaveChangesAsync();
+            }catch (DbUpdateConcurrencyException error) {
+                if (!ProdutoAindaExiste(produtos.Id)) {
+                    return NotFound();
+                } else {
+                    throw;
+                }
+            } catch {
+                return Page();
+            }
 
             return RedirectToPage("./Listar");
+        }
+
+        private bool ProdutoAindaExiste(int? id) {
+            return _context.Produtos.Any(p => p.Id == id);
         }
     }
 }
