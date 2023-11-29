b31ef5c34237f485758c836b6263b2d71202ccc8
KetPR <kethellen48382566@edu.pa.senac.br>
2023-11-29 16:14:22 -0300

    listagem

---
+++ Pages/Listagem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce_CyberKnight.Pages
+{
+    public class ListagemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
