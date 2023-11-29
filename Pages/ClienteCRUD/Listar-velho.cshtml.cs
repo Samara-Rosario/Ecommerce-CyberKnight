d871bcbd81967d2fa8685bd222fc560bcab65339
RafaelG132 <rafael48245716@edu.pa.senac.br>
2023-11-29 17:54:51 -0300

    alterações!

--- Pages/ClienteCRUD/Listar-velho.cshtml.cs
+++ Pages/ClienteCRUD/Listar-velho.cshtml.cs
@@ -7,16 +7,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce_CyberKnight.Pages.ClienteCRUD
 {
-    public class ListarModel : PageModel
+    public class ListarModelVelho : PageModel
     {
-
         private readonly ApplicationDbContext _context;
         public readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
 
 
-        public ListarModel(ApplicationDbContext context, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
-        {
+        public ListarModelVelho(ApplicationDbContext context, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager) {
             _context = context;
             _userManager = userManager;
             _roleManager = roleManager;
@@ -24,27 +22,23 @@ namespace Ecommerce_CyberKnight.Pages.ClienteCRUD
 
         public IList<Clientes> Clientes { get; set; } = new List<Clientes>();
 
-        public async Task<IActionResult> OnGet()
-        {
+        public async Task<IActionResult> OnGet() {
             Clientes = await _context.Clientes.ToListAsync();
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPostDeleteAsync(int? id)
-        {
-
-            if (id == null)
-            {
+        public async Task<IActionResult> OnPostDeleteAsync(int? id) {
+            
+            if (id == null) {
                 return NotFound();
             }
-
+            
             //Busca no banco de dados o cliente com o mesmo id procurado
             var clienteParaDeletar = await _context.Clientes.FirstOrDefaultAsync(c => c.Id == id);
 
             //Verifica se foi retornado algum cliente do banco de dados
-            if (clienteParaDeletar != null)
-            {
+            if (clienteParaDeletar != null) {
                 _context.Clientes.Remove(clienteParaDeletar);
                 await _context.SaveChangesAsync();
             }
@@ -53,20 +47,16 @@ namespace Ecommerce_CyberKnight.Pages.ClienteCRUD
             return RedirectToPage("./Listar");
         }
 
-        public async Task<IActionResult> OnPostDelAdminAsync(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> OnPostDelAdminAsync(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var cliente = await _context.Clientes.FindAsync(id);
 
-            if (cliente != null)
-            {
+            if (cliente != null) {
                 AppUser usuario = await _userManager.FindByNameAsync(cliente.Email);
-                if (usuario != null)
-                {
+                if (usuario != null) {
                     await _userManager.RemoveFromRoleAsync(usuario, "admin");
                 }
             }
@@ -74,20 +64,16 @@ namespace Ecommerce_CyberKnight.Pages.ClienteCRUD
             return RedirectToPage("./Listar");
         }
 
-        public async Task<IActionResult> OnPostSetAdminAsync(int? id)
-        {
-            if (id == null)
-            {
+        public async Task<IActionResult> OnPostSetAdminAsync(int? id) {
+            if (id == null) {
                 return NotFound();
             }
 
             var cliente = await _context.Clientes.FindAsync(id);
 
-            if (cliente != null)
-            {
+            if (cliente != null) {
                 AppUser usuario = await _userManager.FindByNameAsync(cliente.Email);
-                if (usuario != null)
-                {
+                if (usuario != null) {
                     if (!await _roleManager.RoleExistsAsync("admin"))
                         await _roleManager.CreateAsync(new IdentityRole("admin"));
 
