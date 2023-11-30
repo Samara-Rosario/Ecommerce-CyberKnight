129cb5277c9bcfc4f619a486e954d524ba68404f
Samara Rosario <samara48240196@edu.pa.senac.br>
2023-11-30 16:54:12 -0300

    implementando a tela de listagem de produtos

--- Pages/ProdutoCRUD/Listar-velha.cshtml.cs
+++ Pages/ProdutoCRUD/Listar-velha.cshtml.cs
@@ -4,33 +4,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ecommerce_CyberKnight.Pages.ProdutoCRUD
-{
-    public class ListarModel : PageModel
-    {
+namespace Ecommerce_CyberKnight.Pages.ProdutoCRUD {
+    public class Listar2Model : PageModel {
+
         private readonly ApplicationDbContext _context;
 
-        public ListarModel(ApplicationDbContext context) {
+        public Listar2Model(ApplicationDbContext context) {
             _context = context;
         }
 
         public IList<Produto> Produtos { get; set; }
         public async Task<IActionResult> OnGet() {
-            Produtos = await _context.Produtos.ToListAsync();
+           Produtos  = await _context.Produtos.ToListAsync();
 
-            return Page();
+           return Page();
         }
 
         public async Task<IActionResult> OnPostDeleteAsync(int? id) {
             if (id == null) {
                 return NotFound();
             }
-
+            
             //Busca no banco de dados o Produto com o mesmo id procurado
-            var ProdutoParaDeletar = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+			var ProdutoParaDeletar = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
 
-            //Verifica se foi retornado algum Produto do banco de dados
-            if (ProdutoParaDeletar != null) {
+			//Verifica se foi retornado algum Produto do banco de dados
+			if (ProdutoParaDeletar != null) {
                 _context.Produtos.Remove(ProdutoParaDeletar);
                 await _context.SaveChangesAsync();
             }
