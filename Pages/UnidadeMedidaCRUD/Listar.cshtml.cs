64de58d0c89291492efd38726aa43073f7e37bfa
KetPR <kethellen48382566@edu.pa.senac.br>
2023-11-30 17:54:33 -0300

    terminandoooo

--- Pages/UnidadeMedidaCRUD/Listar.cshtml.cs
+++ Pages/UnidadeMedidaCRUD/Listar.cshtml.cs
@@ -4,39 +4,37 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ecommerce_CyberKnight.Pages.UnidadeMedidaCRUD
-{
-    public class ListarModel : PageModel
-    {
-        private readonly ApplicationDbContext _context;
+namespace Ecommerce_CyberKnight.Pages.UnidadeMedidaCRUD {
+            public class ListarModel : PageModel {
+            private readonly ApplicationDbContext _context;
 
-        public ListarModel(ApplicationDbContext context) {
-            _context = context;
-        }
-
-        public IList<UnidadeDeMedida> unidadeMedidas { get; set; }
-        public async Task<IActionResult> OnGet() {
-            unidadeMedidas = await _context.unidadeMedidas.ToListAsync();
-
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostDeleteAsync(int? id) {
-            if (id == null) {
-                return NotFound();
+            public ListarModel(ApplicationDbContext context) {
+                _context = context;
             }
 
-            //Busca no banco de dados o Produto com o mesmo id procurado
-            var UnidadeMedidaParaDeletar = await _context.unidadeMedidas.FirstOrDefaultAsync(u => u.Id == id);
+            public IList<UnidadeDeMedida> unidadeMedidas { get; set; }
+            public async Task<IActionResult> OnGet() {
+                unidadeMedidas = await _context.unidadeMedidas.ToListAsync();
 
-            //Verifica se foi retornado algum Produto do banco de dados
-            if (UnidadeMedidaParaDeletar != null) {
-                _context.unidadeMedidas.Remove(UnidadeMedidaParaDeletar);
-                await _context.SaveChangesAsync();
+                return Page();
             }
 
-            //Redireciona para a página de listagem de Produto
-            return RedirectToPage("./Listar");
+            public async Task<IActionResult> OnPostDeleteAsync(int? id) {
+                if (id == null) {
+                    return NotFound();
+                }
+
+                //Busca no banco de dados o Produto com o mesmo id procurado
+                var UnidadeMedidaParaDeletar = await _context.unidadeMedidas.FirstOrDefaultAsync(u => u.Id == id);
+
+                //Verifica se foi retornado algum Produto do banco de dados
+                if (UnidadeMedidaParaDeletar != null) {
+                    _context.unidadeMedidas.Remove(UnidadeMedidaParaDeletar);
+                    await _context.SaveChangesAsync();
+                }
+
+                //Redireciona para a página de listagem de Produto
+                return RedirectToPage("./Listar");
+            }
         }
     }
-}
